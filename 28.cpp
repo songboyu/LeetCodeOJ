d848b90913e926e77ef51c2807383b51749d3e05
HSS <hss@bluetech.com>
2015-03-02 20:32:32 +0800

    add 28

---
+++ 28.cpp
@@ -0,0 +1,23 @@
+/*
+[Implement strStr()]
+
+Implement strStr().
+
+Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+ */
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int strStr(char *haystack, char *needle) {
+        
+    }
+};
+
+int main(){
+    char* haystack = "asdfweretyrtuytreruyre";
+    char* needle = "re";
+    Solution s;
+    s.strStr(haystack, needle);
+};
